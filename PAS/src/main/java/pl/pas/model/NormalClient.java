16901c0a3511bb1edef3073822a9df217891f067
Patryk Kolanek <marti3go@gmail.com>
2020-11-10 16:31:15 +0100

    Remove old project

--- PAS/src/main/java/pl/pas/model/NormalClient.java
+++
@@ -1,7 +0,0 @@
-package pl.pas.model;
-
-public class NormalClient extends Client {
-    public NormalClient(String name, int age) {
-        super(name, age, 2);
-    }
-}
--- PAS/src/main/java/pl/pas/model/PremiumClient.java
+++
@@ -1,7 +0,0 @@
-package pl.pas.model;
-
-public class PremiumClient extends Client {
-    public PremiumClient(String name, int age) {
-        super(name, age, 10);
-    }
-}
