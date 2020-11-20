9bafd5441c9140cd175864222fcf5bcc4fb985e0
Kacper <kacper.swiercz00@gmail.com>
2020-11-20 15:37:19 +0100

    Add user managing methods

--- PAS/src/main/java/pl/pas/model/Manager.java
+++ PAS/src/main/java/pl/pas/model/Manager.java
@@ -9,11 +9,18 @@ import pl.pas.model.user.Employee;
 import pl.pas.model.user.User;
 
 import java.util.ArrayList;
+import java.util.UUID;
 
 public class Manager {
     UserRepository userRepository;
     ResourceRepository resourceRepository;
     BorrowRepository borrowRepository;
+
+    public enum UserType {
+        CLIENT,
+        EMPLOYEE,
+        ADMINISTRATOR
+    }
 
     public Manager() {
         this.userRepository = new UserRepository();
@@ -22,39 +29,58 @@ public class Manager {
     }
 
     public boolean addUser(String login, String name, String lastName, UserType userType) {
-        if (userType == UserType.CLIENT) {
-            return false;
-        }
-
-        return addUser(login, name, lastName, 0, userType);
+        return userRepository.add(createUser(login, name, lastName, userType));
     }
 
     public boolean addUser(String login, String name, String lastName, int age, UserType userType) {
-        if (login == null || name == null || lastName == null) {
-            return false;
-        }
-
-        switch (userType) {
-            case CLIENT:
-                return userRepository.add(new Client(login, name, lastName, age));
-            case EMPLOYEE:
-                return userRepository.add(new Employee(login, name, lastName));
-            case ADMINISTRATOR:
-                return userRepository.add(new Administrator(login, name, lastName));
-        }
-
-        return false;
+        return userRepository.add(createUser(login, name, lastName, age, userType));
     }
 
     public ArrayList<User> getAllUsers() {
         return userRepository.getAll();
     }
 
-    public enum UserType {
-        CLIENT,
-        EMPLOYEE,
-        ADMINISTRATOR
+    public User getUser(UUID id) {
+        return userRepository.get(id);
     }
+
+    public User getUser(String login) {
+        return userRepository.get(login);
+    }
+
+    public boolean updateUser(User oldUser, String login,  String name, String lastName, UserType userType) {
+        return userRepository.update(oldUser.getUserId(), createUser(login, name, lastName, userType));
+    }
+
+    public boolean updateUser(User oldUser, String login,  String name, String lastName, int age, UserType userType) {
+        return userRepository.update(oldUser.getUserId(), createUser(login, name, lastName, age, userType));
+    }
+
+    private User createUser(String login,  String name, String lastName, UserType userType) {
+        if (userType == UserType.CLIENT) {
+            return null;
+        }
+
+        return createUser(login, name, lastName, 0, userType);
+    }
+
+    private User createUser(String login,  String name, String lastName, int age, UserType userType) {
+        if (login == null || name == null || lastName == null) {
+            return null;
+        }
+
+        switch (userType) {
+            case CLIENT:
+                return new Client(login, name, lastName, age);
+            case EMPLOYEE:
+                return new Employee(login, name, lastName);
+            case ADMINISTRATOR:
+                return new Administrator(login, name, lastName);
+        }
+
+        return null;
+    }
+
 
 
 }
