e78789381fcb07fc3c9e2034fa5ef3033ac8a562
Kacper <kacper.swiercz00@gmail.com>
2020-11-22 14:47:46 +0100

    Add methods to BorrowManager

--- PAS/src/main/java/pl/pas/model/managers/BorrowManager.java
+++ PAS/src/main/java/pl/pas/model/managers/BorrowManager.java
@@ -8,6 +8,7 @@ import pl.pas.model.repositories.interfaces.IBorrowRepository;
 import pl.pas.model.repositories.interfaces.IResourceRepository;
 import pl.pas.model.repositories.interfaces.IUserRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public class BorrowManager {
@@ -24,6 +25,9 @@ public class BorrowManager {
     public boolean borrowResource(UUID resourceId, UUID clientId) {
         Resource resource = resourceRepository.getResource(resourceId);
         User user = userRepository.getUser(clientId);
+        if(resource == null || user == null) {
+            return false;
+        }
 
         if (resource.isAvailable() && user instanceof Client && user.isActive()) {
             borrowRepository.addBorrow(new Borrow((Client) user, resource), UUID.randomUUID());
@@ -33,4 +37,46 @@ public class BorrowManager {
 
         return false;
     }
+
+    public List<Borrow> getAllBorrows() {
+        return borrowRepository.getAllBorrows();
+    }
+
+    public List<Borrow> getAllBorrowsForClient(Client client) {
+        return getAllBorrowsForClient(client.getUserId());
+    }
+
+    public List<Borrow> getAllBorrowsForClient(UUID uuid) {
+        return borrowRepository.getBorrowsByUser(uuid);
+    }
+
+    public List<Borrow> getAllBorrowsForResource(Resource resource) {
+        return getAllBorrowsForResource(resource.getResourceId());
+    }
+
+    public List<Borrow> getAllBorrowsForResource(UUID uuid) {
+        return borrowRepository.getBorrowsByResource(uuid);
+    }
+
+    public boolean endBorrow(Borrow borrow) {
+        if (borrow == null
+                || borrow.getReturnDate() != null
+                || borrowRepository.getBorrow(borrow.getBorrowId()) == null) {
+            return false;
+        }
+        borrow.getResource().setAvailable(true);
+        borrowRepository.endBorrow(borrow.getBorrowId());
+        return true;
+    }
+
+    public boolean cancelBorrow(Borrow borrow) {
+        if (borrow == null
+                || borrow.getReturnDate() != null
+                || borrowRepository.getBorrow(borrow.getBorrowId()) == null) {
+            return false;
+        }
+
+        return  borrowRepository.deleteBorrow(borrow.getBorrowId());
+    }
+
 }
