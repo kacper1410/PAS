f19440504295b9b42c08e5de0b63a3a0cfb0fa79
Daniel Łondka <224360@edu.p.lodz.pl>
2020-11-30 16:40:34 +0100

    add new Client page and connection, not tested

--- PAS/src/main/java/pl/pas/controllers/UserController.java
+++ PAS/src/main/java/pl/pas/controllers/UserController.java
@@ -1,9 +1,10 @@
 package pl.pas.controllers;
 
 import pl.pas.managers.UserManager;
+import pl.pas.model.user.Administrator;
 import pl.pas.model.user.Client;
+import pl.pas.model.user.Employee;
 
-import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -16,6 +17,43 @@ public class UserController implements Serializable {
     @Inject
     private UserManager userManager;
     private Client currentClient;
+    private Client newClient;
+    private Employee newEmployee;
+    private Administrator newAdministrator;
+
+    public UserController() {
+        newClient = new Client();
+        newEmployee = new Employee();
+        newAdministrator = new Administrator();
+    }
+
+    public UserManager getUserManager() {
+        return userManager;
+    }
+
+    public Client getNewClient() {
+        return newClient;
+    }
+
+    public Employee getNewEmployee() {
+        return newEmployee;
+    }
+
+    public Administrator getNewAdministrator() {
+        return newAdministrator;
+    }
+
+    public void setNewClient(Client client) {
+        newClient = client;
+    }
+
+    public void setNewEmployee(Employee employee) {
+        newEmployee = employee;
+    }
+
+    public void setNewAdministrator(Administrator administrator) {
+        newAdministrator = administrator;
+    }
 
     public void setCurrentClient(Client currentClient) {
         this.currentClient = currentClient;
@@ -31,12 +69,38 @@ public class UserController implements Serializable {
         return "client";
     }
 
-
-    public UserManager getUserManager() {
-        return userManager;
-    }
-
     public String userList() {
         return "userList";
+    }
+
+    public String processNewClient() {
+        userManager.addClient(newClient.getLogin(), newClient.getName(), newClient.getLastName(), newClient.getAge());
+        newClient = new Client();
+        return "main";
+    }
+    public String processNewEmployee() {
+        userManager.addEmployee(newEmployee.getLogin(), newEmployee.getName(), newEmployee.getLastName());
+        newEmployee = new Employee();
+        return "main";
+    }
+    public String processNewAdministrator() {
+        userManager.addAdministrator(newAdministrator.getLogin(), newAdministrator.getName(), newAdministrator.getLastName());
+        newAdministrator = new Administrator();
+        return "main";
+    }
+
+    public String cancelNewClient() {
+        newClient = new Client();
+        return "main";
+    }
+
+    public String cancelNewEmployee() {
+        newEmployee = new Employee();
+        return "main";
+    }
+
+    public String cancelNewAdministrator() {
+        newAdministrator = new Administrator();
+        return "main";
     }
 }
--- PAS/src/main/java/pl/pas/model/user/Client.java
+++ PAS/src/main/java/pl/pas/model/user/Client.java
@@ -3,6 +3,11 @@ package pl.pas.model.user;
 public class Client extends User {
     private int age;
 
+    public Client() {
+        super();
+        age = 0;
+    }
+
     public Client(String login, String name, String lastName, int age) {
         super(login, name, lastName);
         this.age = age;
