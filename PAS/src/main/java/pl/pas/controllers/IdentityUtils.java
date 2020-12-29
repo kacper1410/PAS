6fc5b533c99e0e701d4f50f912d6dd23eff16055
Nyneq <londka99@gmail.com>
2020-12-29 18:02:48 +0100

    Remove rendering of remove / change buttons from book, audiobook and resources for appropriate access levels

--- PAS/src/main/java/pl/pas/controllers/IdentityUtils.java
+++ PAS/src/main/java/pl/pas/controllers/IdentityUtils.java
@@ -2,7 +2,6 @@ package pl.pas.controllers;
 
 import pl.pas.managers.UserManager;
 import pl.pas.model.user.User;
-
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
