7458de071c1b2a6155d57e623df10462cbf2c52a
Patryk Kolanek <marti3go@gmail.com>
2021-01-12 16:26:56 +0100

    Add GET methods from ResourceManager

--- PAS/src/main/java/pl/pas/ApplicationConfig.java
+++ PAS/src/main/java/pl/pas/ApplicationConfig.java
@@ -3,6 +3,6 @@ package pl.pas;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("resources")
+@ApplicationPath("api")
 public class ApplicationConfig extends Application {
 }
--- PAS/src/main/java/pl/pas/services/ResourceService.java
+++ PAS/src/main/java/pl/pas/services/ResourceService.java
@@ -2,13 +2,13 @@ package pl.pas.services;
 
 import lombok.NoArgsConstructor;
 import pl.pas.managers.ResourceManager;
+import pl.pas.model.resource.AudioBook;
+import pl.pas.model.resource.Book;
 import pl.pas.model.resource.Resource;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
@@ -21,8 +21,36 @@ public class ResourceService {
     private ResourceManager resourceManager;
 
     @GET
+    @Path("getAllResources")
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
     public List<Resource> getAllResources() {
         return resourceManager.getAllResources();
+    }
+
+    @GET
+    @Path("getAllAvailableResources")
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public List<Resource> getAllAvailableResources() {
+        return resourceManager.getAllAvailableResources();
+    }
+
+    @GET
+    @Path("getAllBooks")
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public List<Book> getAllBooks() {
+        return resourceManager.getAllBooks();
+    }
+
+    @GET
+    @Path("getAllAudioBooks")
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public List<AudioBook> getAllAudioBooks() {
+        return resourceManager.getAllAudioBooks();
+    }
+
+    @DELETE
+    @Path("/removeResource/{uuid}")
+    public void removeResource(@PathParam("uuid") long uuid) {
+        resourceManager.removeResource(uuid);
     }
 }
