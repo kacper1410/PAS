111c69549b0a4957469e654d146489b4d9e6bcb2
Daniel Łondka <224360@edu.p.lodz.pl>
2021-01-30 15:03:44 +0100

    Add IdentityStore

---
+++ PAS/src/main/java/pl/pas/rest/security/InMemoryIdentityStore.java
@@ -0,0 +1,40 @@
+package pl.pas.rest.security;
+
+import pl.pas.model.user.User;
+import pl.pas.repositories.UserRepository;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.security.enterprise.credential.Credential;
+import javax.security.enterprise.credential.UsernamePasswordCredential;
+import javax.security.enterprise.identitystore.CredentialValidationResult;
+import javax.security.enterprise.identitystore.IdentityStore;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationScoped
+public class InMemoryIdentityStore implements IdentityStore {
+
+    @Inject
+    private UserRepository userRepository;
+
+    @Override
+    public CredentialValidationResult validate(Credential credential) {
+        if (credential instanceof UsernamePasswordCredential) {
+            UsernamePasswordCredential usernamePasswordCredential = (UsernamePasswordCredential) credential;
+            User user = userRepository.getUserByLoginPasswordActive(usernamePasswordCredential.getCaller(),
+                    usernamePasswordCredential.getPasswordAsString());
+            if (user != null) {
+                return new CredentialValidationResult(user.getLogin(), new HashSet<>(Arrays.asList(user.getAccessGroup())));
+            }
+            return CredentialValidationResult.INVALID_RESULT;
+        }
+        return CredentialValidationResult.NOT_VALIDATED_RESULT;
+    }
+
+    @Override
+    public Set<String> getCallerGroups(CredentialValidationResult validationResult) {
+        return IdentityStore.super.getCallerGroups(validationResult);
+    }
+}
