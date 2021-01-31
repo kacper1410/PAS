1777a25087833f509100c3b82e084586c706fe67
Patryk Kolanek <marti3go@gmail.com>
2021-01-31 09:10:02 +0100

    Add CR test for users

--- SPA/e2e/cypress/integration/examples/users.spec.js
+++ SPA/e2e/cypress/integration/examples/users.spec.js
@@ -1,10 +1,81 @@
-
 describe('Users test', () => {
   let jwt;
 
-  beforeEach(() => cy.request('POST', '/authenticate', { login: "login", password: "spa" })
-    .its('body')
-    .then((body) => {
-      jwt = body;
-    }))
+  let userId;
+  let name;
+  let login;
+  let lastName;
+
+  beforeEach(() => {
+    cy.request('POST', '/authenticate', {login: "login", password: "spa"})
+      .its('body')
+      .then((body) => {
+        jwt = body;
+      })
+  })
+
+  beforeEach(() => {
+    cy.request({
+      method: 'GET',
+      url: '/user/getAllActiveClients',
+      headers: {
+        'Authorization': 'Bearer ' + jwt
+      }
+    }).its('body')
+      .then((users) => {
+        userId = users[0].userId;
+        name = users[0].name;
+        login = users[0].login;
+        lastName = users[0].lastName;
+      })
+  })
+
+  it('Create user', () => {
+    let user = {
+      login: 'test-client' + Math.random() * 10000,
+      name: 'test',
+      lastName: 'client',
+      age: 20
+    }
+
+    cy.request({
+      method: 'POST',
+      url: '/user/addClient',
+      body: user,
+      headers: {
+        'Authorization': 'Bearer ' + jwt
+      }
+    }).then((response) => {
+      expect(response.status).equal(204)
+    })
+
+    cy.request({
+      method: 'GET',
+      url: '/user/getUserByLogin/' + user.login,
+      headers: {
+        'Authorization': 'Bearer ' + jwt
+      }
+    }).then((response) => {
+      expect(response.status).equal(200)
+      expect(response.body.login).equal(user.login)
+      expect(response.body.name).equal(user.name)
+      expect(response.body.lastName).equal(user.lastName)
+      expect(response.body.age).equal(user.age)
+    })
+  })
+
+  it('Read user', () => {
+    cy.request({
+      method: 'GET',
+      url: '/user/getUserById/' + userId,
+      headers: {
+        'Authorization': 'Bearer ' + jwt
+      }
+    }).then((response) => {
+      expect(response.status).equal(200)
+      expect(response.body.name).equal(name)
+      expect(response.body.login).equal(login)
+      expect(response.body.lastName).equal(lastName)
+    })
+  })
 })
